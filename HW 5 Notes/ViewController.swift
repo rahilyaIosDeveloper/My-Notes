748118c4a5a0c663edfff93aa75e19009e40ae87
rahilyanz <rahaomurzakova@gmail.com>
2024-04-27 17:56:40 +0600

    Initial Commit

---
+++ HW 5 Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 5 Notes
+//
+//  Created by Rahilya Nazaralieva on 27/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
